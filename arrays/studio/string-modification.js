6d9e66fb443ef7f3ec188516bcdd2ff36c68d161
kavitha1808 <kavitha.sekarp@gmail.com>
2024-03-29 08:54:50 -0500

    local changes

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,20 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let newstring = str.slice(0,3);
+console.log(newstring);
+
+let newstring2 = str.slice(3);
+console.log(newstring2);
+
+let newstring3 = newstring2 + newstring
+console.log(newstring3);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`Template literal ${newstring3}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let userName = readlineSync.question('Hello,Please ');
+
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,4 +1,5 @@
 // Declare and assign the variables below
+//console.log('This is Kavitha');
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
@@ -9,3 +10,100 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
+
+console.log(typeof shuttleName)
+console.log(typeof shuttleSpeedMph)
+console.log(typeof distanceToMarsKm)
+console.log(typeof distanceToMoonKm)
+console.log(typeof milesPerKm)
+let kilometersToMars = 225000000
+let milesPerKilometer = 0.621
+let milesToMars = kilometersToMars * milesPerKilometer;
+console.log(typeof milesToMars)
+let hoursToMars = milesToMars / shuttleSpeedMph;
+console.log(typeof hoursToMars)
+let daysToMars = hoursToMars / 24;
+console.log(typeof daysToMars)
+console.log(typeof 3 == 4)
+console.log(3 + 4 === 7)
+console.log("false")
+console.log(typeof 7 == "7");
+console.log(typeof 0 == false);
+console.log(typeof 0 == '')
+console.log(typeof 7 === "7");
+console.log(typeof 0 === false);
+console.log(typeof 0 === '');
+console.log(typeof 4 == "4");
+console.log(shuttleName + " will take "+ daysToMars + " days to reach Mars.");
+console.log(`${shuttleName} will take ${daysToMars} days to reach Mars.`);
+let kilometersToMoon  = 38400;
+let milesToMoon = kilometersToMoon * milesPerKilometer;
+console.log(milesToMoon)
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+console.log(hoursToMoon)
+let daysToMoon = hoursToMoon / 24;
+console.log(daysToMoon)
+console.log(`${shuttleName} will take ${daysToMoon} days to reach the Moon.`);
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+ } else if (engineIndicatorLight === "green blinking") {
+    console.log("engines are preparing to start");
+ } else {
+    console.log("engines are off");
+ }
+ if (crewStatus) {
+    console.log("Crew Ready");
+  } else {
+    console.log("Crew Not Ready");
+  }
+  if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }let num = 7;
+
+ if (num % 2 === 0) {
+     if (num % 2 === 1) {
+         console.log("odd");
+     }
+ }
+ if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
+
+
+let jsCreator = "Brendan Eich";
+let firstInitial = jsCreator[0];
+let lastInitial = jsCreator[8];
+
+let outputStr = "JavaScript was created by somebody with initials " + 
+   firstInitial + "." +
+   lastInitial + ".";
+
+console.log(outputStr);
+
+console.log(jsCreator[-1]);
+console.log(jsCreator[42]);
+let phrase = 'Code for fun'
+console.log(phrase2)
+console.log(phrase[phrase.length - 8]);
